11731a176fd8b80f435385255fa4b202e1ddd2cf
mat-eyo <maciej.matejko@wp.eu>
2016-08-18 12:39:15 +0200

    validation fix

--- api/orderController.php
+++ api/orderController.php
@@ -4,6 +4,11 @@ require_once(__DIR__.'/src/conn.php');
 require_once(__DIR__.'/src/Order.php');
 
 if($_SERVER["REQUEST_METHOD"]==="POST"){
+    if(intval($_POST['quantity'])<=0 || floatval($_POST['price'])<=0.00){
+        $jsonRespond=  json_encode(['status'=>'fail', 'reason'=>'quantity or price is 0 or lesser']);
+        echo($jsonRespond);
+    }
+    else{
         $orderToAdd = new Order();
         $orderToAdd->setQuantity($_POST['quantity']);
         $orderToAdd->setPrice($_POST['price']);
@@ -14,5 +19,6 @@ if($_SERVER["REQUEST_METHOD"]==="POST"){
         else{
             $jsonRespond=  json_encode(['status'=>'fail']);
             echo($jsonRespond);
-        } 
+        }
+    } 
 }
